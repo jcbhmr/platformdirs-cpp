a73ae9d7f4a87f5affcbab68457e7b2d0480bd12
Jacob Hummer <jcbhmr@outlook.com>
2024-08-08 10:37:13 -0500

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -55,7 +55,7 @@ fi
 if ! command -v clang-format; then
     cd ~/.local/bin
     sudo wget https://cosmo.zip/pub/cosmos/bin/clang-format
-    chmod +x clang-format
+    sudo chmod +x clang-format
     echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
     export PATH="$PATH:$HOME/.local/bin"
     source ~/.bashrc
